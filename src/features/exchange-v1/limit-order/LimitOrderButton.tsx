927a29589d5a35a9b8f877c1dbff1dc291fa2952
BitcoinIsCash <tangoswapcash@gmail.com>
2022-09-29 17:26:34 +0200

    fix: orders

--- src/features/exchange-v1/limit-order/LimitOrderButton.tsx
+++ src/features/exchange-v1/limit-order/LimitOrderButton.tsx
@@ -39,6 +39,7 @@ import axios from 'axios'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import CashAddressInput from '../../../components/Input/Cashaddress'
 import useGetOrdersLocal from '../../../hooks/useGetOrdersLocal'
+import { WrappedTokenInfo } from '../../../state/lists/wrappedTokenInfo'
 
 interface LimitOrderButtonProps extends ButtonProps {
   currency: Currency
@@ -199,35 +200,26 @@ const LimitOrderButton: FC<LimitOrderButtonProps> = ({ currency, color, ...rest 
 
     const rawDueTime = expirePicosecondsBN._hex // returns a hex, in the call to Sign the transaction it formats to number automatically.
 
-    let outputValue = parsedAmounts[Field.OUTPUT]?.toSignificant(6)
-    let inputValue = parsedAmounts[Field.INPUT]?.toSignificant(6)
-    let openOrderToLocalStorage = {
-      id: Date.now(),
-      account,
-      // input: {value: inputValue, currency: currencies[Field.INPUT]?.tokenInfo || {...currencies[Field.INPUT], address: currencies[Field.INPUT]?.wrapped.address}},
-      // output: {value: outputValue, currency: currencies[Field.OUTPUT]?.tokenInfo || currencies[Field.OUTPUT]},
-      input: {value: inputValue, currency: {...currencies[Field.INPUT], address: currencies[Field.INPUT]?.wrapped.address}},
-      output: {value: outputValue, currency: currencies[Field.OUTPUT]},
-
-      orderExpiration: orderExpiration.label,
-      dueTime: rawDueTime,
-      status: 'open',
-      rate: Number((parseFloat(outputValue) / parseFloat(inputValue))?.toFixed(2))
-    }
-    // console.log('openOrderToLocalStorage: ', openOrderToLocalStorage)
-
     try {
       let outputValue = parsedAmounts[Field.OUTPUT]?.toSignificant(6)
       let inputValue = parsedAmounts[Field.INPUT]?.toSignificant(6)
+      let inputCurrency
+      let outputCurrency
+      if (currencies[Field.INPUT] instanceof WrappedTokenInfo) {
+        inputCurrency = (currencies[Field.INPUT] as WrappedTokenInfo).tokenInfo
+      } else {
+        inputCurrency = currencies[Field.INPUT]
+      }
+      if (currencies[Field.OUTPUT] instanceof WrappedTokenInfo) {
+        outputCurrency = (currencies[Field.OUTPUT] as WrappedTokenInfo).tokenInfo
+      } else {
+        outputCurrency = currencies[Field.OUTPUT]
+      }
       let openOrderToLocalStorage = {
         id: Date.now(),
         account,
-        // input: {value: inputValue, currency: currencies[Field.INPUT]?.tokenInfo || {...currencies[Field.INPUT], address: currencies[Field.INPUT]?.wrapped.address}},
-        // output: {value: outputValue, currency: currencies[Field.OUTPUT]?.tokenInfo || currencies[Field.OUTPUT]},
-
-        input: {value: inputValue, currency: {...currencies[Field.INPUT], address: currencies[Field.INPUT]?.wrapped.address}},
-        output: {value: outputValue, currency: currencies[Field.OUTPUT]},
-
+        input: {value: inputValue, currency: inputCurrency || {...currencies[Field.INPUT], address: currencies[Field.INPUT]?.wrapped.address}},
+        output: {value: outputValue, currency: outputCurrency || currencies[Field.OUTPUT]},
         orderExpiration: orderExpiration.label,
         dueTime: rawDueTime,
         status: 'open',
